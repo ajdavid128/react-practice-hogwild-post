942ea09b8dd2372d59927fdff7ae37097b21110f
Aaron David <ajdavid128@gmail.com>
2023-03-14 15:15:38 -0500

    mapped over pig to create cards in separate component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import Nav from "./Nav";
+import { useState, useEffect } from "react";
 
 import hogs from "../porkers_data";
+import PigCards from "./PigCards";
 
 function App() {
+
+	const [pigFetch, setPigFetch] = useState([]);
+
+	const pigCardArray = hogs.map((pig) => {
+		return <PigCards key={pig.id} {...pig} /> 
+	}) 
+
 	return (
 		<div className="App">
 			<Nav />
---
+++ src/components/PigCards.js
@@ -0,0 +1,11 @@
+
+
+function PigCards() {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default PigCards;
