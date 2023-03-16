ce1189a949d43d0940616f6ab276bac11e7b5062
Aaron David <ajdavid128@gmail.com>
2023-03-16 10:30:46 -0500

    starting from scratch

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
-import { useState, useEffect } from "react";
 
 import hogs from "../porkers_data";
-import PigCards from "./PigCards";
-import Filter from "./Filter";
+
 
 function App() {
 
-	const [filter, setFilter] = useState(false)
-	const [greased, setGreased] = useState(false)
-
-
-
-	// console.log(hogs)
-
-	const pigCardArray = hogs.map((pig) => {
-		return <PigCards key={pig.name} pig={pig} /> 
-	}) 
 
 	return (
-		<div className="ui grid container App">
-			<div className="sixteen wide column centered">
-				<Nav />
-			</div>
-			<div className="sixteen wide column centered">
-				<Filter 
-					hogs={hogs} 
-					setFilter={setFilter} 
-					filter={filter}
-					greased={greased}
-					setGreased={setGreased}
-				/>
-			</div>
-			<div className="sixteen wide column centered">
-				{pigCardArray}
-			</div>
+		<div className="App">
+			<Nav />
 		</div>
 	);
 }
