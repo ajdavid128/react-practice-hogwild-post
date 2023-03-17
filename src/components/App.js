894f67073d13300aad2ddc70d82f7a3d7a8e4a01
Aaron David <ajdavid128@gmail.com>
2023-03-17 10:28:37 -0500

    practicng forms

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,25 @@ import Nav from "./Nav";
 import PigList from './PigList'
 import Filter from "./Filter";
 
+import Form from "./Form";
+
 import hogs from "../porkers_data";
 
 
 function App() {
 
+	const onFirstLastName = (newInfo) => {
+		
+	}
 
 
 	return (
 		<div className="App">
 			<Nav />
+			<Form />
+			<div>
+
+			</div>
 			<Filter />
 			<PigList hogs={hogs} />
 		</div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -8,22 +8,40 @@ function Form() {
         lastName: ""
     })
 
+    const handleChange = (e) => {
+        console.log(e.target.value)
+        const key = e.target.name;
+        const value = e.target.value;
+
+        setFormInfo({
+            ...formInfo, [key]: value
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(formInfo)
+    }
+
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="first name"
+                    name="firstName"
                     id="firstName"
-                    value=""
-                
+                    value={formInfo.firstName}
+                    onChange={handleChange}
                 />
                 <input
                     type="text"
                     placeholder="last name"
+                    name="lastName"
                     id="lastName"
-                    value=""
+                    value={formInfo.lastName}
+                    onChange={handleChange}
                 />
                 <button>Cool</button>
             </form>
